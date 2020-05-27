ae18949f2526a2cbf879d9312b5b100b50f021e9
Thea Barnes <thea@awesomebarnes.com>
2020-05-27 17:25:31 -0400

    More work on relations

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,3 +1,5 @@
 class Volunteer < ApplicationRecord
   belongs_to :resume
+
+  has_many :highlights, as: :job, dependent: :destroy
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,5 @@
 class Work < ApplicationRecord
   belongs_to :resume
+
+  has_many :highlights, as: :job, dependent: :destroy
 end
