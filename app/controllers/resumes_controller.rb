8e7877ce1ff2468fe7a0d273eccf893b753b19e4
Thea Barnes <thea@awesomebarnes.com>
2020-06-06 18:02:17 -0400

    More UX work

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,5 +1,5 @@
 class ResumesController < ApplicationController
-  before_action :set_resume, only: [:show, :edit, :update, :destroy]
+  before_action :set_resume, only: [:show]
 
   # GET /resumes
   # GET /resumes.json
