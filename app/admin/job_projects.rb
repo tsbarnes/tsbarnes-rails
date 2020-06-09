eb04f9fcfb6948c74b8a8c2e57234cbfba792238
Thea Barnes <thea@awesomebarnes.com>
2020-06-09 17:14:23 -0400

    More work on job projects

--- app/admin/job_projects.rb
+++ app/admin/job_projects.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register JobProject do
   permit_params :job_type, :job_id, :project_id
+
+  form do |f|
+    f.inputs "Job Project" do
+      f.input :job_identifier, collection: (Work.all + Volunteer.all).map { |i| [ i.position, "#{i.class.to_s}-#{i.id}"] }
+      f.input :project, collection: Project.all
+    end
+    f.actions
+  end
 end
--- app/models/job_project.rb
+++ app/models/job_project.rb
@@ -1,4 +1,19 @@
 class JobProject < ApplicationRecord
   belongs_to :job, polymorphic: true
   belongs_to :project
+
+  validates :job, presence: true
+
+  def job_identifier
+    return "#{self.job.class.to_s}-#{self.job.id}" if job.present?
+    return nil
+  end
+
+  def job_identifier=(job_data)
+    if job_data.present?
+      job_data = job_data.split('-')
+      self.job_type = job_data[0]
+      self.job_id = job_data[1]
+    end
+  end
 end
