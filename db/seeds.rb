bf54494ff8a105938999ab7665260ff460560e22
Thea Barnes <thea@tsbarnes.com>
2021-01-13 10:39:29 -0500

    Minor work

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-location = Location.create()
-resume = Resume.create(first_name: "Test", last_name: "Test", location: location)
-skill = Skill.create(resume: resume, name: "Test")
-interest = Interest.create(resume: resume, name: "Test")
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
