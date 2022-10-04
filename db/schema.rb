5e7b8a4665b310132dcfa678f029c67991bc11b3
Thea Barnes <thea@tsbarnes.com>
2022-10-04 04:53:10 -0400

    DB updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_03_132636) do
-
+ActiveRecord::Schema[7.0].define(version: 2021_08_03_132636) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "timescaledb"
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
     t.string "resource_type"
-    t.bigint "resource_id"
+    t.integer "resource_id"
     t.string "author_type"
-    t.bigint "author_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.integer "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
     t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2021_08_03_132636) do
     t.string "record_type", null: false
     t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
+    t.datetime "created_at", precision: nil, null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
@@ -46,13 +46,13 @@ ActiveRecord::Schema.define(version: 2021_08_03_132636) do
     t.text "metadata"
     t.bigint "byte_size", null: false
     t.string "checksum", null: false
-    t.datetime "created_at", null: false
+    t.datetime "created_at", precision: nil, null: false
     t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
+    t.integer "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -61,19 +61,19 @@ ActiveRecord::Schema.define(version: 2021_08_03_132636) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
   create_table "article_categories", force: :cascade do |t|
-    t.bigint "article_id", null: false
-    t.bigint "category_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.integer "article_id", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_article_categories_on_article_id"
     t.index ["category_id"], name: "index_article_categories_on_category_id"
   end
@@ -82,29 +82,29 @@ ActiveRecord::Schema.define(version: 2021_08_03_132636) do
     t.string "title"
     t.string "summary"
     t.string "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "summary"
     t.string "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "courses", force: :cascade do |t|
-    t.bigint "education_id", null: false
+    t.integer "education_id", null: false
     t.string "name"
     t.integer "order"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["education_id"], name: "index_courses_on_education_id"
   end
 
   create_table "educations", force: :cascade do |t|
-    t.bigint "resume_id", null: false
+    t.integer "resume_id", null: false
     t.string "institution"
     t.string "area"
     t.string "school_url"
@@ -112,77 +112,77 @@ ActiveRecord::Schema.define(version: 2021_08_03_132636) do
     t.date "start_date"
     t.date "end_date"
     t.float "gpa"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["resume_id"], name: "index_educations_on_resume_id"
   end
 
   create_table "highlights", force: :cascade do |t|
     t.string "job_type"
-    t.bigint "job_id"
+    t.integer "job_id"
     t.string "description"
     t.integer "order"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["job_type", "job_id"], name: "index_highlights_on_job_type_and_job_id"
   end
 
   create_table "interests", force: :cascade do |t|
-    t.bigint "resume_id", null: false
+    t.integer "resume_id", null: false
     t.string "name"
     t.integer "order"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["resume_id"], name: "index_interests_on_resume_id"
   end
 
   create_table "job_projects", force: :cascade do |t|
     t.string "job_type", null: false
-    t.bigint "job_id", null: false
-    t.bigint "project_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.integer "job_id", null: false
+    t.integer "project_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["job_type", "job_id"], name: "index_job_projects_on_job_type_and_job_id"
     t.index ["project_id"], name: "index_job_projects_on_project_id"
   end
 
   create_table "keywords", force: :cascade do |t|
     t.string "owner_type"
-    t.bigint "owner_id"
+    t.integer "owner_id"
     t.string "name"
     t.integer "order"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["owner_type", "owner_id"], name: "index_keywords_on_owner_type_and_owner_id"
   end
 
   create_table "languages", force: :cascade do |t|
-    t.bigint "resume_id", null: false
+    t.integer "resume_id", null: false
     t.string "name"
     t.string "fluency"
     t.integer "order"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["resume_id"], name: "index_languages_on_resume_id"
   end
 
   create_table "profiles", force: :cascade do |t|
-    t.bigint "resume_id", null: false
+    t.integer "resume_id", null: false
     t.string "network"
     t.string "icon_name"
     t.string "username"
     t.string "url"
     t.integer "order"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["resume_id"], name: "index_profiles_on_resume_id"
   end
 
   create_table "project_categories", force: :cascade do |t|
-    t.bigint "project_id", null: false
-    t.bigint "category_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.integer "project_id", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_project_categories_on_category_id"
     t.index ["project_id"], name: "index_project_categories_on_project_id"
   end
@@ -192,18 +192,18 @@ ActiveRecord::Schema.define(version: 2021_08_03_132636) do
     t.string "summary"
     t.string "description"
     t.string "url"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "order"
   end
 
   create_table "references", force: :cascade do |t|
-    t.bigint "resume_id", null: false
+    t.integer "resume_id", null: false
     t.string "name"
     t.string "reference"
     t.integer "order"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["resume_id"], name: "index_references_on_resume_id"
   end
 
@@ -212,8 +212,8 @@ ActiveRecord::Schema.define(version: 2021_08_03_132636) do
     t.string "email"
     t.string "phone"
     t.string "summary"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "name"
     t.string "address"
     t.string "city"
@@ -224,10 +224,10 @@ ActiveRecord::Schema.define(version: 2021_08_03_132636) do
   end
 
   create_table "service_categories", force: :cascade do |t|
-    t.bigint "service_id", null: false
-    t.bigint "category_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.integer "service_id", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_service_categories_on_category_id"
     t.index ["service_id"], name: "index_service_categories_on_service_id"
   end
@@ -237,19 +237,19 @@ ActiveRecord::Schema.define(version: 2021_08_03_132636) do
     t.string "price"
     t.string "summary"
     t.string "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "order"
   end
 
   create_table "skills", force: :cascade do |t|
-    t.bigint "resume_id", null: false
+    t.integer "resume_id", null: false
     t.string "name"
     t.string "url"
     t.string "level"
     t.integer "order"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["resume_id"], name: "index_skills_on_resume_id"
   end
 
@@ -257,37 +257,37 @@ ActiveRecord::Schema.define(version: 2021_08_03_132636) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "volunteers", force: :cascade do |t|
-    t.bigint "resume_id", null: false
+    t.integer "resume_id", null: false
     t.string "organization"
     t.string "position"
     t.string "website"
     t.date "start_date"
     t.date "end_date"
     t.string "summary"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["resume_id"], name: "index_volunteers_on_resume_id"
   end
 
   create_table "works", force: :cascade do |t|
-    t.bigint "resume_id", null: false
+    t.integer "resume_id", null: false
     t.string "company"
     t.string "position"
     t.string "website"
     t.date "start_date"
     t.date "end_date"
     t.string "summary"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["resume_id"], name: "index_works_on_resume_id"
   end
 
